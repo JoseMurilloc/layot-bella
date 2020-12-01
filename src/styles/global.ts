1b9638cbba86c897ba5b9ea5742ecca387448f49
José Murillo <jooseemurillo@gmail.com>
2020-12-01 14:17:08 -0300

    Updated css global reset

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,8 +7,20 @@ export default createGlobalStyle`
     box-sizing: border-box; 
   }
 
-  body {
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
     font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  } 
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
   }
 
   button {
